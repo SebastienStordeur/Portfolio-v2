86e829e166c620764bce79986ca8690ec832ee62
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-07-24 18:17:32 +0200

    404 page redirect to homepage, footer mobile done

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import FooterLinks from './FooterLinks'
 
 const Footer = () => {
   return (
-    <footer id='footer' className='flex flex-col w-full bg-background h-12'>
-        <FooterLinks />
-        test
+    <footer id='footer' className='mt-16 flex flex-col w-full bg-background h-32 pb-8'>
+      <FooterLinks />
+      <p className='flex justify-center m-11 text-white font-medium'>2022© Designed and built by <span className='text-primary ml-1 font-semibold'>Sebastien Stordeur</span></p>
     </footer>
   )
 }
--- components/Projects/ProjectCard/Project.jsx
+++ components/Projects/ProjectCard/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
     <Card>
         <header className='flex items-center justify-between'>
             <div className='folder'>
-                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="w-12 text-white"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="w-12 text-primary"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             </div>
             <div className='project-link cursor-pointer'>
                 {props.project.live && <Link href={props.project.live}>
